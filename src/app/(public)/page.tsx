a6cbf01ff62bb977ddf6cd7c90d391797857f46e
nakanohidekatsu <hidekatsu.nakano@gmail.com>
2025-04-15 01:30:17 +0900

    formatTime対応

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -107,7 +107,7 @@ function InnerComponent() {
 
   }, [timeLeft, timerActive, paused]);
 
-  const formatTime = (sec) => {
+  const formatTime = (sec: number): string => {
     const m = Math.floor(sec / 60).toString().padStart(2, "0");
     const s = (sec % 60).toString().padStart(2, "0");
     return `${m}:${s}`;
