a6f73900fb52c1ad549920df6cd50fa62191eace
nakanohidekatsu <hidekatsu.nakano@gmail.com>
2025-04-15 22:45:42 +0900

    comment対応11

--- src/app/(menu)/music/page.tsx
+++ src/app/(menu)/music/page.tsx
@@ -41,7 +41,7 @@ export default function MusicPage() {
     try {
 //      const res = await fetch(`${process.env.NEXT_PUBLIC_API_POINT}/get_misic?souund_id=${musicId}`);
 //      console.error(`nakano:/get_misic?souund_id=${musicId}`);
-//      console.error(`nakano:`,process.env.NEXT_PUBLIC_API_ENDPOINT);
+      console.error(`nakano:`,process.env.NEXT_PUBLIC_API_ENDPOINT);
       const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + `/get_misic?souund_id=${musicId}`);
 //      const res = await fetch(`https://app-002-step3-2-py-oshima9.azurewebsites.net/get_misic?souund_id=${musicId}`);
       if (!res.ok) throw new Error("音楽が見つかりません");
