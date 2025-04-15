abfc5db8d33ad3d50efecce62646015b7787be35
nakanohidekatsu <hidekatsu.nakano@gmail.com>
2025-04-15 17:29:17 +0900

    server.jp対応

--- src/server.js
+++ src/server.js
@@ -1,14 +1,6 @@
-// app.js
-import express from 'express';
-const app = express();
 
 // 環境変数 PORT が設定されている場合はその値、なければ 3000 番ポートを使用
 const PORT = process.env.PORT || 3000;
-
-// シンプルなルートエンドポイントを定義
-app.get('/', (req, res) => {
-  res.send('Hello World! This is my Node.js application.');
-});
 
 // サーバーの起動
 app.listen(PORT, () => {
