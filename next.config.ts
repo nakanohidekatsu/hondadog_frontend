4ed9998357fc9b8fcc998d56d2f562f36528ff3d
nakanohidekatsu <hidekatsu.nakano@gmail.com>
2025-04-15 17:52:36 +0900

    yml対応

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+/**};
 
-export default nextConfig;
+export default nextConfig; */
+
+/** @type
+{import('next’).NextConfig} */
+const nextConfig = {
+  output: 'standalone'}
+  module.exports = nextConfig
